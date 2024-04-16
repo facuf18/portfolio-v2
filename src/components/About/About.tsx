fdaf8b442b779dc5c56ca081b787d66a41a8635e
facuf18 <facufaccioli18@gmail.com>
2024-04-16 20:07:55 -0300

    about me message

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,27 +6,24 @@ export default function About() {
       </h4>
       <div className='justify-center grid-cols-2 gap-6 px-4 pt-2 sm:px-10 md:px-32 sm:grid'>
         <p className='col-span-1 font-light text-justify font-body'>
-          I am a web developer with a passion for programming and continuous
-          learning. I trained at Henry and Coderhouse, two institutions that
-          gave me the knowledge and tools necessary to create dynamic,
-          interactive, and responsive web applications using JavaScript, React,
-          Redux, Node.js, Express, PostgreSQL, Sequelize, Material-UI,
-          Tailwindcss, and other technologies. I have knowledge of both frontend
-          and backend. However, I am currently specializing in front-end.
-          Currently, I work as a Web Developer at Nexo Solutions, a company that
-          offers innovative and personalized digital solutions to its clients.
-          There, I participate in the development, maintenance, and improvement
-          of various web projects, in front-end, applying the best code, design,
-          and usability practices.
+          I am Facundo, a passionate web developer with a strong enthusiasm for
+          programming and continuous learning. I completed my training at Henry
+          and Coderhouse, two renowned institutions that equipped me with the
+          knowledge and tools necessary to create dynamic, interactive, and
+          responsive web applications using JavaScript, TypeScript, React,
+          Next.js, and other cutting-edge technologies. I possess expertise in
+          both frontend and backend development, but I am currently specializing
+          in frontend development.
         </p>
         <p className='col-span-1 font-light text-justify font-body'>
-          Previously, I was part of the Henry team, where I developed two
-          projects: Play Center, a social network for gamers with features such
-          as chat, missions, rewards, and payment gateway; and Countries App, a
-          web application that displays information about countries, with
-          pagination, filtering, search, and creation. These projects allowed me
-          to demonstrate and consolidate my technical skills, as well as my
-          ability to work in a team, communicate, and solve problems.
+          I am currently employed as a Web Developer at Nexo Soluciones, a
+          company that provides innovative and customized digital solutions to
+          its clients. In this role, I actively participate in the development,
+          maintenance, and enhancement of diverse frontend web projects,
+          adhering to the best practices in code, design, and usability. These
+          projects have allowed me to showcase and solidify my technical skills,
+          as well as my ability to collaborate effectively, communicate clearly,
+          and solve problems efficiently.
         </p>
       </div>
     </section>
