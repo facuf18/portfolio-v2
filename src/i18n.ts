5ff66e21c6f751417275276ea3e743a293347c38
facuf18 <facufaccioli18@gmail.com>
2024-04-16 22:32:13 -0300

    text abour - favicon

--- src/i18n.ts
+++ src/i18n.ts
@@ -52,9 +52,9 @@ const resources = {
       about: {
         title: 'Sobre mí',
         firstText:
-          'I am Facundo, a passionate web developer with a strong enthusiasm for programming and continuous learning. I completed my training at Henry and Coderhouse, two renowned institutions that equipped me with the knowledge and tools necessary to create dynamic, interactive, and responsive web applications using JavaScript, TypeScript, React, Next.js, and other cutting-edge technologies. I possess expertise in both frontend and backend development, but I am currently specializing in frontend development.',
+          'Soy Facundo, desarrollador web apasionado por la programación y el aprendizaje continuo. Me formé en Henry y Coderhouse, dos instituciones que me brindaron los conocimientos y las herramientas necesarias para crear aplicaciones web dinámicas, interactivas y responsivas, utilizando JavaScript, Typescript, React, Next.js, entre otras tecnologías. Poseo conocimientos tanto de frontend como backend. Pero en este momento me especializo en front.',
         secondText:
-          'I am currently employed as a Web Developer at Nexo Soluciones, a company that provides innovative and customized digital solutions to its clients. In this role, I actively participate in the development, maintenance, and enhancement of diverse frontend web projects, adhering to the best practices in code, design, and usability. These projects have allowed me to showcase and solidify my technical skills, as well as my ability to collaborate effectively, communicate clearly, and solve problems efficiently.',
+          'Actualmente, trabajo como Web Developer en Nexo Soluciones, una empresa que ofrece soluciones digitales innovadoras y personalizadas a sus clientes. Allí, participo en el desarrollo, mantenimiento y mejora de diversos proyectos web, en frontend, aplicando las mejores prácticas de código, diseño y usabilidad.  Estos proyectos me permitie demostrar y afianzar mis habilidades técnicas, así como mi capacidad de trabajo en equipo, comunicación y resolución de problemas.',
       },
       experience: {
         title: 'Experiencia y Proyectos',
